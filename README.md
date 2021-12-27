9274be70a68baa6ce4bcad96d1b2c09e14efd8b5
oscar <275611070@qq.com>
2021-12-27 15:55:22 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mwget
+
+```bash
+sudo apt install intltool
+git clone git@github.com:ruanhailiang/mwget.git
+cd ./mwget/mwget_0.1.0.orig
+./configure
+make
+sudo make install
+```
+
---
+++ mwget_0.1.0.orig/mach-info/config.h
@@ -0,0 +1,4 @@
+#define BUILT_TIME "2021-12-27 15:26:55"
+#define CC_VERSION "cc (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+#define CXX_VERSION "c++ (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+#define SYSTEM_TYPE "GNU/Linux x86_64"
